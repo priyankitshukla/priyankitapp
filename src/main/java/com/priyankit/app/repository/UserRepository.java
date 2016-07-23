5902e6485bbc66d1255d29396fe0f90889f9bd6f
aradhya <aradhya@aradhya-pc>
2016-07-23 19:38:59 +0530

    Mysql Configuration Updated.

--- src/main/java/com/priyankit/app/repository/UserRepository.java
+++ src/main/java/com/priyankit/app/repository/UserRepository.java
@@ -2,9 +2,10 @@
 package com.priyankit.app.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.priyankit.app.model.User;
-
+@Repository
 public interface UserRepository extends CrudRepository< User, String>{
 	  
 	  public User findByUsername(String username); 
--- src/main/java/com/priyankit/app/service/AuthUserDetailsService.java
+++ src/main/java/com/priyankit/app/service/AuthUserDetailsService.java
@@ -11,11 +11,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.priyankit.app.repository.UserRepository;
 
-@Repository
+@Service
 public class AuthUserDetailsService implements UserDetailsService {
   @Autowired
   private UserRepository users;
