0ec8284b468329682809b1b0c7e6f113fd27ee78
aradhya <aradhya@aradhya-pc>
2016-07-23 19:52:34 +0530

    Wrong Import corrected.

--- src/main/java/com/priyankit/app/model/User.java
+++ src/main/java/com/priyankit/app/model/User.java
@@ -1,21 +1,24 @@
 
 package com.priyankit.app.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.data.annotation.Id;
 @Entity
 @Table(name = "user")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private String id;
-  
+  @Column(nullable = false)
   private String username;
+  @Column(nullable = false)
   private String password;
+  @Column(nullable = false)
   private int role;
   
   public User(){}
