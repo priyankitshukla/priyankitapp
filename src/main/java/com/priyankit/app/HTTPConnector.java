c85edb21b08438490dbb484b748e810d0965eecc
aradhya <aradhya@aradhya-pc>
2016-07-24 16:15:18 +0530

    Httpconnector and Https configuration added, now project port will be 8443

---
+++ src/main/java/com/priyankit/app/HTTPConnector.java
@@ -0,0 +1,30 @@
+package com.priyankit.app;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.SocketUtils;
+
+@Configuration
+public class HTTPConnector {
+
+	@Bean
+	public Integer port() {
+		return SocketUtils.findAvailableTcpPort();
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(port());
+		return connector;
+}
+}
